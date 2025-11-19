e145fa6a338062fe7f6841b72a81503dc5f231c8
Yi-sir <969299937@qq.com>
2025-11-19 17:35:18 +0800

    add fix chunk size test

--- test/mha_mla_prefill.py
+++ test/mha_mla_prefill.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 all_params = {
     "batch_size": [8],
-    "prompt_len": [512, 1024, 2048, 4096, 8192, 16384],
+    "prompt_len": [16384 * 2],
     "cached_len": [1],
     "q_head_num": [128],
     "kv_head_num": [128],
@@ -29,10 +29,12 @@ all_params = {
     "v_head_dim": [128],
     "page_size": [1],
     "tp": [1, 8],
-    "attn_type": ["mha", "mla"],
+    "attn_type": ["mha"],
 }
 
 ATTN_BACKENDS = [TritonMHAPrefill, FA3Prefill]
+PREFILL_CHUNKED_CAPACITY = 32768
+SEQ_LENS = [1024, 1024 * 2, 1024 * 4, 1024 * 8, 1024 * 16, 1024 * 32]
 
 def get_varying_params(params_list):
     if not params_list:
@@ -48,10 +50,8 @@ def get_varying_params(params_list):
 
     return sorted(varying_params)
 
-if __name__ == "__main__":
 
-    device = torch.device("cuda:2")
-
+def test_varing_params(device: torch.device):
     attns = [backend(device) for backend in ATTN_BACKENDS]
 
     params_list = generate_params(all_params)
@@ -62,6 +62,7 @@ if __name__ == "__main__":
         config_parts = [f"{key}={param[key]}" for key in varying_params]
         row = [", ".join(config_parts)]
         for attn in attns:
+            logger.info(f"profiling attn {attn._kernel_name} with params {param}...")
             attn.set_params(param)
             t = attn.profile_kernel_us()
 
@@ -71,3 +72,45 @@ if __name__ == "__main__":
 
     headers = ["Config"] + [attn._kernel_name + "(ms)" for attn in attns]
     print(tabulate(results, headers=headers, tablefmt="grid"))
+
+
+def test_with_fix_chunk_capacity(device: torch.device):
+    batch_size = [PREFILL_CHUNKED_CAPACITY // seqlen for seqlen in SEQ_LENS]
+    tps = [1, 8]
+    param = all_params.copy()
+    param["batch_size"] = [-1]
+    param["prompt_len"] = [-1]
+    param["tp"] = [-1]
+
+    param = generate_params(param)[0]
+
+    attn = FA3Prefill(device)
+    results = []
+
+    for (bs, seqlen) in zip(batch_size, SEQ_LENS):
+        param["batch_size"] = bs
+        param["prompt_len"] = seqlen
+        for tp in tps:
+            param["tp"] = tp
+            config_parts = [f"{key}={param[key]}" for key in ["batch_size", "prompt_len", "tp"]]
+            row = [", ".join(config_parts)]
+
+            logger.info(f"profiling attn {attn._kernel_name} with params {param}...")
+
+
+            attn.set_params(param)
+            t = attn.profile_kernel_us()
+            t_ms = t / 1000
+            row.append(f"{t_ms:.3f}")
+            results.append(row)
+
+    headers = ["Config", attn._kernel_name + "(ms)"]
+    print(tabulate(results, headers=headers, tablefmt="grid"))
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cuda:2")
+
+    # test_varing_params(device)
+    test_with_fix_chunk_capacity(device)
